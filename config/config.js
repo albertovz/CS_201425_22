06d3f6b845d938e50d0fc5e2cf8f62e20e289031
albertovz <201425@ids.upchiapas.edu.mx>
2022-06-22 08:44:58 -0500

    config upgraded

--- config/config.js
+++ config/config.js
@@ -1,19 +1,20 @@
 import path from 'path';
 import dotenv from 'dotenv';
-import { fileURLToPath } from 'url';
+import {fileURLToPath} from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const data = dotenv.config({
     path: path.resolve(__dirname, `../environments/.env.${process.env.NODE_ENV}`)
-});
+})
+
+console.log(process.env.USERS);
 
 export const db = {
-    user: data.parsed.USER,
-    host: data.parsed.HOST,
-    database: data.parsed.DATABASE,
-    password: data.parsed.PASSWORD
+    user: process.env.USERS,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
 };
 
 export const api = {
