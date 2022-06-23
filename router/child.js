626b6721f19f1260dd17dadbc7ed1097008b3593
albertovz <201425@ids.upchiapas.edu.mx>
2022-06-23 02:41:55 -0500

    delete & update added to children

--- router/child.js
+++ router/child.js
@@ -33,4 +33,38 @@ router.post('/add_child', async function (req, res) {
         });
 });
 
+router.put('/update', async function (req, res) {
+
+    let id = req.query.id;
+    let newDatas = req.query;
+
+    getChildren.findOne({ where: { id: id } })
+        .then((r) => {
+            console.log('\nnew data: ', newDatas.password)
+            newDatas.password = newDatas.password && newDatas.password != "" ? bcrypt.hashSync(newDatas.password, 10) : "";
+            r.update(newDatas)
+            //   success(req, res, r, 200);
+            res.send(newDatas)
+            // })
+            // .catch((e) => {
+            //   success(req, res, e, 400);
+        })
+});
+
+router.delete('/delete', async function (req, res) {
+    let id = req.query.id;
+    getChildren.destroy({
+        where: { id: id }
+    }).then(function (rowDeleted) { // rowDeleted will return number of rows deleted
+        if (rowDeleted === 1) {
+            res.send(console.log('Deleted successfully'));
+        } else {
+            res.send(console.log('No exists'));
+        }
+    }, function (err) {
+        console.log(err);
+    });
+    //res.send(req.query);
+});
+
 export default router;
