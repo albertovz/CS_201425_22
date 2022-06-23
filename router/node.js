68244ff98b9b7a0edde4d906d2a0f51c1936fe71
albertovz <201425@ids.upchiapas.edu.mx>
2022-06-23 02:33:28 -0500

    delete & update added to node

--- router/node.js
+++ router/node.js
@@ -16,12 +16,12 @@ router.get('/get_all_nodes', async function (req, res) {
 
 router.post('/add_node', async function (req, res) {
     getNode.create({
-        
+
         name: req.query.name,
         last_name: req.query.last_name,
         second_surname: req.query.second_surname,
         age: req.query.age,
-        id_user : req.query.id_user
+        id_user: req.query.id_user
 
     }, { fields: ['name', 'last_name', 'second_surname', 'age', 'id_user'] })
         .then(users => {
@@ -31,6 +31,40 @@ router.post('/add_node', async function (req, res) {
             res.send(err)
             console.log(err)
         });
+});
+
+router.put('/update', async function (req, res) {
+
+    let id = req.query.id;
+    let newDatas = req.query;
+
+    getNode.findOne({ where: { id: id } })
+        .then((r) => {
+            console.log('\nnew data: ', newDatas.password)
+            newDatas.password = newDatas.password && newDatas.password != "" ? bcrypt.hashSync(newDatas.password, 10) : "";
+            r.update(newDatas)
+            //   success(req, res, r, 200);
+            res.send(newDatas)
+            // })
+            // .catch((e) => {
+            //   success(req, res, e, 400);
+        })
+});
+
+router.delete('/delete', async function (req, res) {
+    let id = req.query.id;
+    getNode.destroy({
+        where: { id: id }
+    }).then(function (rowDeleted) { // rowDeleted will return number of rows deleted
+        if (rowDeleted === 1) {
+            console.log('Deleted successfully');
+        } else {
+            console.log('No exists');
+        }
+    }, function (err) {
+        console.log(err);
+    });
+    res.send(req.query);
 });
 
 export default router;
